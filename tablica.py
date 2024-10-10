7ca908056a3de10c5a4b36dcb963e581573c2ed9
student17 <student17@teb.pl>
2024-10-10 11:21:58 +0200

    commit 1

--- tablica.py
+++ tablica.py
@@ -3,11 +3,10 @@ import random
 
 class Tablica:
     def __init__(self, n):
-        self.tablica = [0] * n
+        self.tablica = [n]
 
     def wypelnij(self, min_zakres, max_zakres):
-        for i in range(self.tablica):
-            self.tablica.append(random.randint(min_zakres, max_zakres))
+
 
     def wyswietl(self):
         print(self.tablica)
